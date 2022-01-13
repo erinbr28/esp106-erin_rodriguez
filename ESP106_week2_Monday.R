ee73cd045a53c740c955ec0916b64b421b7cf318
Erin Rodriguez <ebrrodriguez@ucdavis.edu>
2022-01-12 21:55:24 -0800

    added ESP106_week2_Monday.R

---
+++ ESP106_week2_Monday.R
@@ -0,0 +1,51 @@
+## This is the lab for week 2. 
+## Add working R code in between the questions
+## The chapters refer to the sections in http://rspatial.org/intr/
+
+## Chapter 8  (Functions)
+
+## 1) Write a function 'f' that behaves like this:
+## > f('Jim')
+## [1] "hello Jim, how are you?"
+greeting <- function(name) {
+  paste("hello ", name, ", how are you?", sep = "")
+}
+greeting("erin")
+
+## 2) Write a function 'sumOfSquares' that behaves like this:
+
+
+## > d <- c(1,5,2,4,6,2,4,5)
+## > sumOfSquares(d)
+## [1] 21.875
+
+# To compute the "sum of squares", subtract the mean value of all numbers from each number. 
+# Square these numbers and sum them
+# (stretch goal: make a variant that can handle NA values - no extra points, just a challenge)
+d <- c(1,5,2,4,6,2,4,5)
+
+sumOfSquares <- function(d) {
+  temp <- d-mean(d, na.rm=TRUE)
+  sum((temp*temp), na.rm=TRUE)
+}
+
+sumOfSquares(d)
+
+## Chapter 10  (Flow control)
+
+## 3) Write a for loop that adds the numbers 1 to 10
+j <- 0
+for (i in 1:10) {
+  j <- j+i
+}
+j
+
+## 4) Write a for loop that adds the odd numbers beween 1 and 10 
+odd <- 0
+for (i in 1:10) {
+  if ((i%%2)==1) {
+    print(i)
+    odd <- odd+i
+  }
+}
+odd
